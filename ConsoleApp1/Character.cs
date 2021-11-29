45c53b8321aacb22897eed05fab7178917cb6dae
Nastia <anastasiiamarets@nltu.lviv.ua>
2021-11-29 11:01:57 +0200

    Add project files.

---
+++ ConsoleApp1/Character.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class Character
+    {
+        String typeofcharacter = "Unknown character";
+        String name = "Unknown";
+        int power = 1;
+        public String Typeofcharacter
+        {
+            set { typeofcharacter = value; }
+        }
+        public String Name
+        {
+            set { name = value; }
+        }
+        public virtual String GetTypeofcharacter()
+        {
+            return typeofcharacter;
+        }
+        public virtual String GetName()
+        {
+            return name;
+        }
+
+        public override string ToString()
+        {
+            return "Тип персонажа - " + GetTypeofcharacter() + Environment.NewLine + "Ім'я -" + GetName();
+        }
+    }
+}
+}
